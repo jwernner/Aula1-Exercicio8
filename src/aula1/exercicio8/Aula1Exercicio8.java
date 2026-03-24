89c0aeb676e24135c421bf3332c4ca4bbc13f32e
jwernner <jorge.werner@gmail.com>
2026-03-24 16:32:52 -0300

    projeto

--- src/aula1/exercicio8/Aula1Exercicio8.java
+++ src/aula1/exercicio8/Aula1Exercicio8.java
@@ -4,6 +4,8 @@
  */
 package aula1.exercicio8;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jwern
@@ -15,6 +17,13 @@ public class Aula1Exercicio8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int numero1,numero2,soma;
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+        soma = numero1 + numero2;
+        JOptionPane.showMessageDialog(null,"O resultado da soma é: " + soma);
+
     }
     
 }
